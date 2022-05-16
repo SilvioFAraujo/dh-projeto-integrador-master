5201f22d2f520ed6d9a1df97391fe2a6cf0eb039
Silvio Fernandes <silvio.fernandesjr@live.com>
2022-05-16 14:24:16 -0300

    Página procurar

---
+++ controllers/PoliticasController.js
@@ -0,0 +1,88 @@
+const PoliticasController = {
+    index: (req, res) => {
+        let info = {
+            titulo: 'Programa AirPod Para Sempre',
+        };
+        return res.render("index", info);
+    },
+
+    sobreNos: (req, res) => {
+        let info = {
+            titulo: 'Sobre Nós',
+        };
+        return res.render("sobre-nos", info)
+    },
+
+    termosECondicoes: (req, res) => {
+        let info = {
+            titulo: 'Termos e Condições',
+        };
+        return res.render("termos-e-condicoes", info)
+    },
+
+    politicaDePrivacidade: (req, res) => {
+        let info = {
+            titulo: 'Política de Privacidade',
+        };
+        return res.render("politica-de-privacidade", info)
+    },
+    
+    entregaProdutos: (req, res) => {
+        let info = {
+            titulo: 'Entrega dos Produtos',
+        };
+        return res.render("entrega-produtos", info)
+    },
+
+    painelUsuario: (req, res) => {
+        let info = {
+            titulo: 'Painel do Usuario',
+        };
+        return res.render("pagina-usuario", info)
+    },
+
+    login: (req, res) => {
+        let info = {
+            titulo: 'Login',
+        };
+        return res.render("login", info)
+    },
+
+    carrinho: (req, res) => {
+        let info = {
+            titulo: 'Carrinho',
+        };
+        return res.render("carrinho", info)
+    },
+
+    cadastro: (req, res) => {
+        let info = {
+            titulo: 'Cadastro',
+        };
+        return res.render("cadastro", info)
+    },
+
+    procurar: (req, res) => {
+        let info = {
+            titulo: 'Procurar',
+        };
+        return res.render("procurar", info)
+
+    },
+
+    produto: (req, res) =>{
+        let info = {
+            titulo: "Descricao do produto",
+        };
+        return res.render('descricaoProduto', info)
+    },
+    finalizacao: (req, res) => {
+        let info = {
+            titulo: "Finalização do Pedido",
+        };
+        return res.render('finalizacaoPedido', info)
+    }
+
+}
+
+module.exports = PoliticasController;
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+// var CarrinhoControler = require('../controllers/CarrinhoController');
+var PoliticasController = require('../controllers/PoliticasController');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Programa AirPods Para Sempre' });
+});
+
+// Área dos Produtos
+
+
+
+// Área do Carrinho
+
+// router.get('/:item', CarrinhoControler.addItem);
+// router.get('/:item', CarrinhoControler.removerItem);
+
+// Área do Footer
+
+router.get("/", PoliticasController.index);
+router.get("/sobre-nos", PoliticasController.sobreNos);
+router.get("/termos-e-condicoes", PoliticasController.termosECondicoes);
+router.get("/politica-de-privacidade", PoliticasController.politicaDePrivacidade);
+router.get("/entrega-produtos", PoliticasController.entregaProdutos);
+
+//Arrumar esta rota
+//router.get("/descricaoProduto", ProductsController.descricaoProduto);
+
+
+router.get("/pagina-usuario", PoliticasController.painelUsuario);
+router.get("/login", PoliticasController.login);
+router.get("/carrinho", PoliticasController.carrinho);
+router.get("/cadastro", PoliticasController.cadastro);
+router.get("/procurar", PoliticasController.procurar);
+router.get("/finalizacaoPedido", PoliticasController.finalizacao)
+
+
+//Adicinar rota descricaoProduto.
+
+router.get('/descricaoProduto', PoliticasController.produto)
+
+module.exports = router;
